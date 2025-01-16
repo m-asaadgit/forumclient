cb0ce2d4dae9e6ef67c97d3b583b7b29f161dcb2
m-asaadgit <asaaddangi20@gmail.com>
2025-01-16 16:35:39 +0530

    lander animation added

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,14 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Provider } from 'react-redux';
 import Store from "./redux/Store.jsx"
+import logo from "./assets/logo.png"; // Replace this with the path to your imported image
+
+// Dynamically set the favicon
+const link = document.createElement("link");
+link.rel = "icon";
+link.type = "image/png";
+link.href = logo;
+document.head.appendChild(link);
 
 createRoot(document.getElementById('root')).render(
   <Provider
